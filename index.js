35391fb731ea9dadcd2fa938d8e18c7c182057b4
Rithik Raj <rithu0644@gmail.com>
2023-10-28 21:30:14 +0530

    Initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +21,28 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+let obj = {};
+
+app.get("/api/:input", (req, res) => {
+  dateInput = req.params.input;
+
+  if (dateInput.includes('-')) {
+    obj.unix = new Date(dateInput).getTime();
+    obj.utc = new Date(dateInput).toUTCString();
+  } else {
+    obj.unix = new Date(parseInt(dateInput)).getTime();
+    obj.utc = new Date(parseInt(dateInput)).toUTCString();
+  }
+
+  if (!obj.unix || !obj.utc) {
+    res.json({ error: "Invalid Date" });
+  }
+
+  res.json(obj);
+})
 
 
 // listen for requests :)
